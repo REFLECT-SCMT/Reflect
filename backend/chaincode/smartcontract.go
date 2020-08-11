24d7488e324ac76c0081741e2710134228ab2fb7
SIDD <siddgage@gmail.com>
2020-08-11 15:06:52 +0530

    added queries

--- backend/chaincode/smartcontract.go
+++ backend/chaincode/smartcontract.go
@@ -3,9 +3,13 @@ package chaincode
 import(
 	"encoding/json"
 	"fmt"
+	"time"
 
+	"github.com/hyperledger/fabric-chaincode-go/shim"
 	"github.com/hyperledger/fabric-contract-api/contractapi"
 )
+
+const index = "AssetType~desc"
 
 type SmartContract struct{
 	contractapi.Contract
@@ -14,18 +18,34 @@ type SmartContract struct{
 type Asset struct{
 	ID string `json:"ID"`
 	Owner string `json:"Owner"`
+	AssetType string `json:AssetType`
 	Quantity int `json:"Quantity"`
 	AppraisedValue int `json:"AppraisedValue"`
 }
 
+type HistoryQuery struct{
+	Record *Asset `json:"record"`
+	TxID string `json:"txID"`
+	Timestamp time.Time `json:"timestamp"`
+	Deleted bool `json:"deleted"`
+}
+
+type PaginatedQuery struct{
+	Records []*Asset `json:"records"`
+	FetchedRecordCount int32 `json:"fetechedrecordCount"`
+	Bookmark string `jso:"bookmark"`
+}
+
+
+
 func (sc *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error{
 	assets := []Asset{
-		{ID: "asset1", Owner: "Tomoko", Quantity: 10, AppraisedValue: 300},
-		{ID: "asset2", Owner: "Brad", Quantity: 6, AppraisedValue: 400},
-		{ID: "asset3", Owner: "Jin Soo", Quantity: 18, AppraisedValue: 500},
-		{ID: "asset4", Owner: "Max", Quantity: 2, AppraisedValue: 600},
-		{ID: "asset5", Owner: "Adriana", Quantity: 26, AppraisedValue: 700},
-		{ID: "asset6", Owner: "Michel", Quantity: 8, AppraisedValue: 800},
+		{ID: "asset1", Owner: "Tomoko",AssetType: "household" Quantity: 10, AppraisedValue: 300},
+		{ID: "asset2", Owner: "Brad",AssetType: "industry" Quantity: 6, AppraisedValue: 400},
+		{ID: "asset3", Owner: "Jin Soo",AssetType: "industry" Quantity: 18, AppraisedValue: 500},
+		{ID: "asset4", Owner: "Max",AssetType: "household" Quantity: 2, AppraisedValue: 600},
+		{ID: "asset5", Owner: "Adriana",AssetType: "mines" Quantity: 26, AppraisedValue: 700},
+		{ID: "asset6", Owner: "Michel",AssetType: "mines" Quantity: 8, AppraisedValue: 800},
 	}
 
 	for _, asset := range assets{
@@ -40,23 +60,29 @@ func (sc *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface)
 	return nil
 }
 
-func (sc *SmartContract) CreateAsset(ctx contractapi.TransactionContextInterface, id string, owner string, quantity int, appraisedvalue int) error{
+func (sc *SmartContract) CreateAsset(ctx contractapi.TransactionContextInterface, id, owner, assettype string, quantity, appraisedvalue int) error{
 	exists, err := sc.ExistsAsset(ctx, id);if err != nil{
-		return err
+		return fmt.Errorf("failed to get asset: %v", err)
 	}
 	if exists{
 		return fmt.Errorf("Asset %s already exits", id)
 	}
 
-	asset := Asset{
-		ID : id, Owner : owner, Quantity : quantity, AppraisedValue : appraisedvalue,
+	asset := &Asset{
+		ID : id, Owner : owner, AssetType: assettype, Quantity : quantity, AppraisedValue : appraisedvalue,
 	}
 
 	assetJson, err:= json.Marshal(asset); if err != nil{
 		return err
 	}
 	
-	return ctx.GetStub().PutState(id, assetJson)
+	assetTypeIndexKey, err := ctx.GetStub().CreateCompositeKey(index, []string{asset.AssetType, asset.ID})
+	if err != nil{
+		return err
+	}
+	value := []byte{0x00}
+
+	return ctx.GetStub().PutState(assetTypeIndexKey, value)
 }
 
 func (sc *SmartContract) ReadAsset(ctx contractapi.TransactionContextInterface, id string) (*Asset, error){
@@ -71,10 +97,10 @@ func (sc *SmartContract) ReadAsset(ctx contractapi.TransactionContextInterface, 
 	err = json.Unmarshal(assetJson,&asset); if err != nil{
 		return nil, err
 	}
-	return asset, nil
+	return &asset, nil
 }
 
-func (sc *SmartContract) UpdateAsset(ctx contractapi.TransactionContextInterface, id string, owner string, quantity int, appraisedvalue int) error{
+func (sc *SmartContract) UpdateAsset(ctx contractapi.TransactionContextInterface, id, owner string, quantity, appraisedvalue int) error{
 	exists, err := sc.ExistsAsset(ctx, id);if err != nil{
 		return err
 	}
@@ -102,6 +128,17 @@ func (sc *SmartContract) DelAsset(ctx contractapi.TransactionContextInterface, i
 	}
 
 	return ctx.GetStub().DelState(id)
+	if err != nil {
+		return fmt.Errorf("failed to delete asset %s: %v", id, err)
+	}
+
+	assetTypeIndexKey, err := ctx.GetStub().CreateCompositeKey(index, []string{asset.AssetType, asset.ID})
+	if err != nil {
+		return err
+	}
+
+	// Delete index entry
+	return ctx.GetStub().DelState(colorNameIndexKey)
 }
 
 func (sc *SmartContract) TransAsset(ctx contractapi.TransactionContextInterface, id, newOwner string) error{
@@ -115,11 +152,97 @@ func (sc *SmartContract) TransAsset(ctx contractapi.TransactionContextInterface,
 	return ctx.GetStub().PutState(id, assetJson)
 }
 
-func (sc *SmartContract) ExistsAsset(ctx contractapi.TransactionContextInterface, id string) (bool, error){
-	assetJson, err := ctx.GetStub().GetState(id); if err != nil{
-		return false, fmt.Errorf("failed to read from world state: %v", err)
+func (sc *SmartContract) TransAssetByType(ctx contractapi.TransactionContextInterface, assettype, newOwner string) error{
+	assetTyperesultsIterator, err := ctx.GetStub().GetStateByPartialCompositeKey(index, []string{assettype})
+	if err != nil{
+		return err
 	}
-	return assetJson !=nil, nil
+	defer assetTyperesultsIterator.Close()
+
+	for assetTyperesultsIterator.HasNext(){
+		responseRange, err := assetTyperesultsIterator.Next(); if err != nill{
+			return err
+		}
+		_,compositeKeyParts, err := ctx.GetStub().SliptCompositeKey(responseRange.Key); if err != nil{
+			return err
+		}
+		if len(compositeKeyParts) > 1{
+			returnAssetID := compositeKeyParts[1]
+			asset, err := sc.ReadAsset(ctx, returnAssetID); if err != nil{
+				return err
+			}
+			asset.Owner = newOwner
+			assetJson, err := json.Marshal(asset); if err != nil{
+				return err
+			}
+			err = ctx.GetStub().PutState(returnAssetID, ass); if err != nil{
+				return fmt.Errorf("tranfer of ownership failed for asset %s : &v", returnAssetID, err)
+			}
+		}
+	}
+	return nil
+}
+
+
+func (sc *SmartContract) QueryResponseFromIterator(resultsIterator shim.SateQueryIteratorInterface)([]*Asset,error){
+	var asset []*Asset
+	for resultsIterator.HasNext(){
+		queryResults, err := resultsIterator.Next(); if err != nil{
+			return nil, err
+		}
+		var asset Asset
+		err =json.Unmarshal(queryResults.Value, &asset)
+	}
+	return &asset, nil
+}
+
+func (sc *SmartContract) GetAssetByRange(ctx contractapi.TransactionContextInterface, startkey, endkey string) ([]*Asset, error){
+	resultsIterator, err := ctx.GetStub().GetAssetByRange(startkey, endkey); if err != nil{
+		return nil, err
+	}
+	defer resultsIterator.Close()
+
+	return QueryResponseFromIterator(resultsIterator)
+}
+
+func (sc *SmartContract) QueryAssetByOwner(ctx contractapi.TransactionContextInterface, owner string) ([]*Asset, error){
+	queryString, err := fmt.Sprintf(`{"selector":{"owner": "%s"}}`, owner)
+	return getQueryResultForQueryString(queryString)
+}
+
+func (sc *SimpleChaincode) QueryAssets(ctx contractapi.TransactionContextInterface, queryString string) ([]*Asset, error) {
+	return getQueryResultForQueryString(ctx, queryString)
+}
+
+func (sc *SmartContract) getQueryResultForQueryString(ctx contractapi.TransactionContextInterface,queryString string) ([]*Asset, error){
+	resultsIterator, err := ctx.GetStub(),GetQueryResult(queryString); if err != nil{
+		return err
+	}
+	defer resultsIterator.Close()
+
+	return QueryResponseFromIterator(resultsIterator)
+}
+
+func (sc *SmartContract) getAssetWithPagination(ctx contractapi.TransactionContextInterface, queryString, bookmark string, pageSize int) (*PaginatedQuery, error){
+	return getQueryResultForQueryStringWithPagination(ctx, queryString, bookmark, int32(pageSize))
+}
+
+func (sc *SmartContract) getQueryResultForQueryStringWithPagination(ctx contractapi.TransactionContextInterface, queryString, bookmark string, pageSize int32) (*PaginatedQuery, error){
+	resultsIterator, responseMetadata, err := ctx.GetStub().GetQueryResultWithPagination(queryString,pageSize, bookmark)
+	if err != nil{
+		return err
+	}
+	defer resultsIterator.Close()
+
+	assets, err := QueryResponseFromIterator(resultsIterator); if err != nil {
+		return err
+	}
+
+	return &PaginatedQuery{
+		Records : assets,
+		FetchedRecordCount : responseMetadata.FetchedRecordCount,
+		Bookmark : responseMetadata.bookmark,
+	}, nil
 }
 
 func (sc *SmartContract) QueryAllAsset(ctx contractapi.TransactionContextInterface) ([]Asset, error){
@@ -141,4 +264,44 @@ func (sc *SmartContract) QueryAllAsset(ctx contractapi.TransactionContextInterfa
 		assets := append(assets, asset)
 	}
 	return assets, nil
+}
+
+func (sc *SimpleChaincode) GetAssetHistory(ctx contractapi.TransactionContextInterface, id string) ([]HistoryQuery, error) {
+	resultsIterator, err := ctx.GetStub().GetHistoryForKey(id); if err != nil {
+		return nil, err
+	}
+	defer resultsIterator.Close()
+
+	var records []HistoryQuery
+	for resultsIterator.HasNext() {
+		response, err := resultsIterator.Next()
+		if err != nil {response.Value}
+			return nil, err
+		}
+
+		var asset Asset
+		err = json.Unmarshal(response.Value, &asset); if err != nil {
+			return nil, err
+		}
+
+		timestamp, err := ptypes.Timestamp(response.Timestamp); if err != nil {
+			return nil, err
+		}
+		record := HistoryQueryResult{
+			TxID:      response.TxId,
+			Timestamp: timestamp,
+			Record:    &asset,
+			Deleted:  response.Deleted,
+		}
+		records = append(records, record)
+	}
+
+	return records, nil
+}
+
+func (sc *SmartContract) ExistsAsset(ctx contractapi.TransactionContextInterface, id string) (bool, error){
+	assetJson, err := ctx.GetStub().GetState(id); if err != nil{
+		return false, fmt.Errorf("failed to read from world state: %v", err)
+	}
+	return assetJson !=nil, nil
 }
